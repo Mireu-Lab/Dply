d3ccfd68c805401c3a147bae4d7e4683e07584cd
mireu-lab <limmireu1214@gmail.com>
2023-07-02 02:08:00 +0900

    feat: Installer Code CleanUp 처리

--- installer.sh
+++ installer.sh
@@ -1,4 +1,45 @@
-#!/bin/bash
+#!/bin/sh
+
+pythonInstall() {
+    sudo apt-get update
+    sudo apt-get install python3 python3-pip
+}
+
+dockerInstall() {
+    sudo apt-get update
+    distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+    curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+    sudo apt-get update
+    sudo apt-get install -y docker docker.io docker-compose nvidia-docker2
+    sudo systemctl restart docker
+}
+
+dockerSSHImage() {
+    #SSH Container Image
+    docker pull registry.gitlab.com/container-images4/docker-ssh-conteiner:debiansshcontainer
+    docker pull registry.gitlab.com/container-images4/docker-ssh-conteiner:fedorasshcontainer
+    docker pull registry.gitlab.com/container-images4/docker-ssh-conteiner:rockylinuxsshcontainer
+    docker pull registry.gitlab.com/container-images4/docker-ssh-conteiner:ubuntusshcontainer
+}
+
+dockerJupyterImage() {
+    #Jupyter Container Image
+    docker pull registry.gitlab.com/container-images4/docker-jupyter-container:containerjupyterdebian
+    docker pull registry.gitlab.com/container-images4/docker-jupyter-container:containerjupyterfedora
+    docker pull registry.gitlab.com/container-images4/docker-jupyter-container:containerjupyterrockylinux
+    docker pull registry.gitlab.com/container-images4/docker-jupyter-container:containerjupyterubuntu
+}
+
+dockerDataBaseImage() {
+    # Database Container Image
+    docker pull mysql:latest
+    docker pull mariadb:latest
+    docker pull mongo:latest
+    docker pull redis:latest
+}
+
 
 cat asciiArt
 
@@ -7,31 +48,20 @@ if [ $(id -u) -ne 0 ]; then
     exit
 fi
 
-echo "\n\n\n"
 
-sudo apt-get update
+if cat /etc/issue | grep -q -e Ubuntu -e ubuntu ; then
+    pythonInstall
+    dockerInstall
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+    dockerSSHImage
+    dockerJupyterImage
+    dockerDataBaseImage
 
-sudo apt-get update
-sudo apt-get install -y docker docker.io docker-compose nvidia-docker2
-sudo systemctl restart docker
+    echo "\n\n\n+Complete basic system installation"
 
+    docker-compose up --build -d
 
-#SSH Container
-docker pull registry.gitlab.com/container-images4/docker-ssh-conteiner:debiansshcontainer
-docker pull registry.gitlab.com/container-images4/docker-ssh-conteiner:fedorasshcontainer
-docker pull registry.gitlab.com/container-images4/docker-ssh-conteiner:rockylinuxsshcontainer
-docker pull registry.gitlab.com/container-images4/docker-ssh-conteiner:ubuntusshcontainer
-
-#Jupyter Container
-docker pull registry.gitlab.com/container-images4/docker-jupyter-container:containerjupyterdebian
-docker pull registry.gitlab.com/container-images4/docker-jupyter-container:containerjupyterfedora
-docker pull registry.gitlab.com/container-images4/docker-jupyter-container:containerjupyterrockylinux
-docker pull registry.gitlab.com/container-images4/docker-jupyter-container:containerjupyterubuntu
-
-docker-compose up --build -d
-
-echo "Done!"
+    echo "Done!"
+else
+    echo "\n\nUnsupported OS."
+fi
