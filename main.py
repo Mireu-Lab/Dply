d853a627fb680b753625ef719deb0a7645f0fc4e
mireu-lab <limmireu1214@gmail.com>
2023-07-01 20:49:27 +0900

    feat: container stop, start, restart, remove, build API 기능 추가

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.Container.build import Build as containerBuilder
 from src.error import ERROR
 from src.Container import (
     build as containerBuilder,
@@ -6,51 +7,75 @@ from src.Container import (
 )
 
 from fastapi import FastAPI
-from jsonset import buildOS, databaseBuild, buildType
+from jsonset import buildInfo
 
 
 api = FastAPI()
 
 
 @api.post("/container/build", tags=["Container"])
-async def containerBuild(
-    projectName: str,
-    containerType: buildType = "Jupyter",
-    containerOS: buildOS = "ubuntu",
-    databaseContainer: databaseBuild = None,
-    password: str = None,
-):
+async def containerBuild(buildInfo: buildInfo, processorPlans: int = 0):
     devContainerBuilder = None
-    try:
-        ContainerBuilderClass = containerBuilder.Build(projectName, containerOS, password)
+    databaseContainers = None
 
-        if type(ContainerBuilderClass) == None:
-            match containerType:
-                case "Jupyter":
-                    devContainerBuilder = ContainerBuilderClass.jupyter()
-                    
-                case "SSH":
-                    devContainerBuilder = ContainerBuilderClass.ssh()
+    if containerSearch.containerNameCheck(buildInfo.projectName) == False:
+        ContainerBuilderClass = containerBuilder.Build(
+            processorPlans,
+            buildInfo.Processor,
+            buildInfo.projectName,
+            buildInfo.OS,
+            buildInfo.Password,
+            buildInfo.databaseList,
+        )
+
+        if buildInfo.Type == "Jupyter" or buildInfo.Type == "jupyter":
+            devContainerBuilder = ContainerBuilderClass.jupyter()
+
+        elif buildInfo.Type == "SSH" or buildInfo.Type == "ssh":
+            devContainerBuilder = ContainerBuilderClass.ssh()
+
+        if devContainerBuilder["status"] == True:
+            if buildInfo.databaseList != None:
+                databaseContainers = ContainerBuilderClass.database()
 
             return {
                 "devContainer": {
-                    "status": devContainerBuilder.status,
-                    "port": devContainerBuilder.port,
+                    "status": devContainerBuilder["status"],
+                    "port": devContainerBuilder["port"],
                 },
-                "databaseContainer": ContainerBuilderClass.databaseBuild(databaseContainer),
+                "databaseContainer": databaseContainers,
             }
 
-        else:
-            return ContainerBuilderClass
+    else:
+        return ERROR.API_Error_Messages(
+            503, "NotBuildContainer", "The name is already registered."
+        )
 
-    except:
-        ERROR.Logging()
-        return ERROR.API_Error_Messages(500)
+
+@api.put("/container/stop", tags=["Container"])
+async def containerStop():
+    pass
+
+
+@api.put("/container/start", tags=["Container"])
+async def containerRestart():
+    pass
+
+
+@api.put("/container/restart", tags=["Container"])
+async def containerRestart():
+    pass
 
 
 @api.delete("/container/remove", tags=["Container"])
 async def containerRemove(projectName: str):
-    pass
+    if containerSearch.containerNameCheck(projectName) == True:
+        return containerDelete.remove(projectName).Project()
+
+    else:
+        return ERROR.API_Error_Messages(
+            404, "NotFoundProjects", "Projects that cannot be found"
+        )
 
 
 @api.get("/container/find", tags=["Container"])
@@ -61,4 +86,4 @@ async def containerFind(search: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(api, host="0.0.0.0", port=16170)
+    uvicorn.run(api, host="0.0.0.0", port=8080)
