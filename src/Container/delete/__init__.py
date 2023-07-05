580626f9a5e6c86a77ba363379f283535e2cd4f2
mireu-lab <limmireu1214@gmail.com>
2023-07-06 02:18:24 +0900

    feat: 주석 추가 및 코드 가독성 변경

--- src/Container/delete/__init__.py
+++ src/Container/delete/__init__.py
@@ -4,41 +4,84 @@ from .delete import projectDelete
 
 
 def containerIDFind(self, projectName: str) -> None:
-    DevContainerInfo = DataBase.execute(
-        f"SELECT `DevContainerID`, `Port` FROM `DevContainer` WHERE `DevContainerName` = '{projectName}'"
+    """
+    해당 함수는 개발환경 컨테이너 ID와 데이터 베이스 컨테이너들 ID를 받아 클래스 변수로 지정 해주는 함수이다.
+
+    해당 함수를 실행하기 위해서 필요한 변수는 아래와 같다.
+
+    | 변수명 | 타입 | 기본값 |            설명            |
+    | :----: | :--: | :----: | :--------------------: |
+    | projectName | str |   None   | 프로젝트 이름 |
+
+    결과값으로는 **None**이 출력된다.
+    """
+
+    devContainerInfo = DataBase.execute(
+        f"SELECT `devContainerID`, `port` FROM `devContainer` WHERE `projectName` = '{projectName}'"
     ).fetchall()
 
-    self.DevContainerID = str(DevContainerInfo[0][0])
-    self.Port = int(DevContainerInfo[0][1])
+    self.devContainerID = str(devContainerInfo[0][0])  # 개발환경 컨테이너 ID
+    self.port = int(devContainerInfo[0][1])  # 개발환경 컨테이너 포트
 
-    databaseContainerIDList = DataBase.execute(
-        f"SELECT `DataBaseID` FROM `DatabaseContainer` WHERE `DevContainerID` = '{self.DevContainerID}'"
-    ).fetchall()
-
-    for databaseContainerID in databaseContainerIDList:
-        self.databaseContainerID.append(databaseContainerID[0][0])
+    self.databaseContainerID = DataBase.execute(
+        f"SELECT `databaseType`, `databaseID` FROM `databaseContainer` WHERE `devContainerID` = '{self.devContainerID}'"
+    ).fetchall()  # 데이터베이스 컨테이너들 ID 리스트
 
     return None
 
 
 class remove:
     def __init__(self, projectName: str = None) -> None:
-        self.projectName = projectName
-        self.DevContainerID = None
-        self.Port = 0
-        self.databaseContainerID = []
+        """
+        해당 함수는 프로젝트 컨테이너를 삭제하기 위해 사용되는 Class이다.
+
+        해당 함수에서 필요한 변수는 아래와 같다.
+
+        | 변수명 | 타입 | 기본값 |            설명            |
+        | :----: | :--: | :----: | :--------------------: |
+        | projectName | str |   None   | 프로젝트 이름 |
+        """
+
+        self.projectName = projectName  # 프로젝트 이름
+
+        self.devContainerID = None  # 개발 환경 컨테이너 ID
+        self.port = None  # 개발 환경 컨테이너 포트
+
+        self.databaseContainerID = []  # 프로젝트 데이터베이스 컨테이너 IDs
 
         containerIDFind(self, projectName)
 
         """Projects Docker Container Network Setting ID"""
         self.projectNetworks = DockerClient.networks.get(
-            f"{projectName}_{self.Port}_network"
-        ).short_id
+            f"{projectName}_{self.port}_network"
+        ).id
 
         """Projects Docker Container Volume Setting ID"""
-        self.projectVolumes = DockerClient.volumes.get(
-            f"{projectName}_{self.Port}_volume"
-        ).short_id
+        self.projectVolumes = set(
+            containerID.id
+            for containerID in DockerClient.volumes.list()
+            if (containerID.id).startswith(f"{self.projectName}_{self.port}")
+        )
 
     def Project(self) -> dict:
+        """
+        해당 함수는 프로젝트 컨테이너를 최종적으로 삭제 하기 위한 함수이다.
+
+        필요한 변수는 없으며 출력값으로는 dict으로 출력된다.
+
+        출력 결과값은 아래와 같다.
+
+        ```
+        {
+            "projectName": str,
+            "projectStatus": {
+                "devContainer": bool,
+                "databaseContainers": dict[bool],
+                "containersNetwork": bool,
+                "containersVolume": bool
+            },
+        }
+        ```
+        """
+
         return projectDelete(self)
--- src/Container/delete/delete.py
+++ src/Container/delete/delete.py
@@ -1,42 +1,151 @@
 from src.error import ERROR
-from src.set import DataBase, DockerClient
-
-import time
+from src.set import DataBase, SQL, DockerClient
 
 
-def sqlDelete(self) -> None:
-    DataBase.execute(
-        f"""delete from `DevContainer` where `DevContainerID` = '{self.DevContainerID}';"""
-    )
+def devContainer(self) -> bool:
+    """
+    해당 함수는 개발환경 컨테이너를 삭제 하기 위한 함수이다.
 
-    for databaseContainerID in self.databaseContainerID:
-        DataBase.execute(
-            f"""delete from `DatabaseContainer` where `DevContainerID` = '{databaseContainerID}';"""
-        )
+    결과값으로는 Bool으로 출력된다.
+    """
 
-    return None
-
-
-def projectDelete(self) -> dict:
-    status = False
+    Status = False
 
     try:
-        DockerClient.containers.get(self.DevContainerID).stop()
-        DockerClient.containers.get(self.DevContainerID).remove()
+        """컨테이너 정지후 삭제"""
+        DockerClient.containers.get(self.devContainerID).stop()
+        DockerClient.containers.get(self.devContainerID).remove()
 
-        for databaseContainerID in self.databaseContainerID:
-            DockerClient.containers.get(databaseContainerID).stop()
-            DockerClient.containers.get(databaseContainerID).remove()
+        """SQL 데이터 삭제"""
+        DataBase.execute(
+            f"""delete from `devContainer` where `devContainerID` = '{self.devContainerID}';"""
+        )
+        SQL.commit()
 
-        DockerClient.networks.get(self.projectNetworks).remove()
-        time.sleep(2)
-        DockerClient.volumes.get(self.projectVolumes).remove()
-
-        sqlDelete(self)
-
-        status = True
+        Status = True
 
     except:
         ERROR.Logging()
+        Status = False
 
-    return {"projectName": self.projectName, "status": status}
+    return Status
+
+
+def databaseContainer(self) -> dict:
+    """
+    해당 함수는 데이터베이스 컨테이너를 삭제 하기 위한 함수이다.
+
+    결과값으로는 dict[bool]으로 출력된다.
+
+    출력 결과값으로는 아래와 같이 구성되어있다.
+
+    ```
+    {
+        str: bool,
+        ...
+    }
+    ```
+    """
+
+    databaseContainersStatus = {}
+
+    for databaseContainerID in self.databaseContainerID:
+        Status = False
+
+        try:
+            """컨테이너 정지후 삭제"""
+            DockerClient.containers.get(databaseContainerID[1]).stop()
+            DockerClient.containers.get(databaseContainerID[1]).remove()
+
+            """SQL 데이터 삭제"""
+            DataBase.execute(
+                f"""delete from `databaseContainer` where `devContainerID` = '{self.devContainerID}';"""
+            )
+            SQL.commit()
+
+            Status = True
+
+        except:
+            ERROR.Logging()
+            Status = False
+
+        databaseContainersStatus.update(
+            {databaseContainerID[0]: Status}
+        )  # 컨테이너 삭제 결과값 dict 추가
+
+    return databaseContainersStatus
+
+
+def projectVolume(self) -> bool:
+    """
+    해당 함수는 프로젝트 볼륨 삭제를 하기 위한 함수이다.
+
+    결과값으로는 bool으로 출력된다.
+    """
+
+    Status = False
+
+    try:
+        """볼륨 이름이 프로젝트 이름과 같거나 겹치면 삭제"""
+        for projectVolumes in self.projectVolumes:
+            DockerClient.volumes.get(projectVolumes).remove()  # 볼륨 삭제
+
+        Status = True
+
+    except:
+        ERROR.Logging()
+        Status = False
+
+    return Status
+
+
+def projectNetwork(self):
+    """
+    해당 함수는 프로젝트 네트워크 삭제를 하기 위한 함수이다.
+
+    결과값으로는 bool으로 출력된다.
+    """
+
+    Status = False
+
+    try:
+        DockerClient.networks.get(self.projectNetworks).remove()  # 네트워크 삭제
+        Status = True
+
+    except:
+        ERROR.Logging()
+        Status = False
+
+    return Status
+
+
+def projectDelete(self) -> dict:
+    """
+    해당 함수는 프로젝트 컨테이너, 볼륨, 네트워크를 삭제하는 함수이다.
+
+    필요한 함수는 없으며 결과 타입은 dict이다.
+
+    출력값은 아래와 같다.
+
+    ```
+    {
+        "projectName": str,
+        "projectStatus": {
+            "devContainer": bool,
+            "databaseContainers": dict[bool],
+            "containersNetwork": bool,
+            "containersVolume": bool
+        },
+    }
+    ```
+    """
+
+    return {
+        "projectName": self.projectName,
+        "projectStatus": {
+            "devContainer": devContainer(self),
+            "databaseContainers": databaseContainer(self),
+            "containersNetwork": projectNetwork(self),
+            "containersVolume": projectVolume(self),
+        },
+    }
