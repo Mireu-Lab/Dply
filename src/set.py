c9aea443dddd5c4c94829baac6e20edcbf27d3bd
mireu-lab <limmireu1214@gmail.com>
2023-07-06 02:17:24 +0900

    feat: GPUScheduler, randomPort 함수 build으로 이전

--- src/set.py
+++ src/set.py
@@ -33,59 +33,5 @@ except:
     ERROR.Program_Shutdown()
 
 
-def GPUScheduler() -> int:
-    """
-    GPU 스케줄링 함수입니다.
-    """
-
-    try:
-        gpuDevice = []
-
-        for gpuDevices in torch.cuda.device_count():
-            DataBase.execute(
-                f"select GPU from DevContainer WHERE GPU IS NULL and GPU = {gpuDevices};"
-            )
-            gpuDevice.append(len(DataBase.fetchall()))
-
-        return gpuDevice.index(min(gpuDevice))
-
-    except:
-        ERROR.Logging()
-        return 0
-
-
-def randomPort() -> int | None:
-    """
-    랜덤으로 포트 중복되지 않는 데이터를 출력합니다.
-    """
-
-    import random
-
-    try:
-        returnList = []
-
-        # SQL Container Port Select
-        DataBase.execute("select Port from DevContainer;")
-
-        for row in DataBase.fetchall():
-            returnList.append(row[0])
-
-        # Random Num
-        for _ in range(5):
-            randomNum = random.randint(
-                Setting_ENV["PortSet"]["MIN"], Setting_ENV["PortSet"]["MAX"]
-            )
-
-            returnInt = randomNum if randomNum not in returnList else None
-            if returnInt != None:
-                break
-
-        return returnInt
-
-    except:
-        ERROR.Logging()
-        return None
-
-
 def time() -> float:
     return datetime.datetime.now().timestamp()
