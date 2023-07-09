24677e2939ca715af5ad0a7d34b3c363daa35258
mireu-lab <limmireu1214@gmail.com>
2023-07-09 10:05:17 +0900

    fix: 컨테이너 정지 및 삭제 진행 이슈

--- src/remove.py
+++ src/remove.py
@@ -3,14 +3,14 @@ import docker
 DockerClient = docker.DockerClient()
 
 for containerID in DockerClient.containers.list():
-    if (containerID.id).startswith(f"Build_Management_"):
-        DockerClient.containers.get(containerID.id).stop()
-        DockerClient.containers.get(containerID.id).remove()
+    if (containerID.name).startswith(f"Build_Management_"):
+        DockerClient.containers.get(str(containerID.id)).stop()
+        DockerClient.containers.get(str(containerID.id)).remove()
 
 for containerID in DockerClient.networks.list():
-    if (containerID.id).startswith(f"Build_Management_"):
-        DockerClient.networks.get(containerID.id).remove()
+    if (containerID.name).startswith(f"Build_Management_"):
+        DockerClient.networks.get(str(containerID.name)).remove()
 
 for containerID in DockerClient.volumes.list():
-    if (containerID.id).startswith(f"Build_Management_"):
-        DockerClient.volumes.get(containerID.id).remove()
+    if (containerID.name).startswith(f"Build_Management_"):
+        DockerClient.volumes.get(str(containerID.name)).remove()
